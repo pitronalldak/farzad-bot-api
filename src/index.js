a57c6f9ba2703bb334164b5da492ae4706e0aa73
badbreaddead <pavlovandrew@mail.ru>
2017-08-06 11:12:20 +0800

    https

--- src/index.js
+++ src/index.js
@@ -88,7 +88,7 @@ function connect() {
 
 function listen() {
     if (app.get('env') === 'test') return;
-    app.server.listen(port);
+    app.listen(port);
     console.log('Express app started on port ' + port);
 }
 
