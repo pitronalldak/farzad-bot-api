9ff191fac94b9e1bbf08c215556b829322360786
badbreaddead <pavlovandrew@mail.ru>
2017-08-29 17:01:57 +0800

    logger

---
+++ src/testSSL.js
@@ -0,0 +1,17 @@
+'use strict';
+
+require('letsencrypt-express').create({
+
+  server: 'staging'
+
+  , email: 'john.doe@example.com'
+
+  , agreeTos: true
+
+  , approveDomains: [ 'example.com' ]
+
+  , app: require('express')().use('/', function (req, res) {
+    res.end('Hello, World!');
+  })
+
+}).listen(80, 443);
